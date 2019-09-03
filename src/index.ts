0f4afc6cbb8503c10f7e06d3a5885c572dddcf2b
Simp-lexx <imlexx007@gmail.com>
2019-09-03 12:21:31 +1000

    add persistent task store

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import * as inquirer from "inquirer";
 
 import { TodoCollection } from "./todoCollection";
 import { TodoItem } from "./todoItem";
+import { JsonTodoCollection } from "./jsonTodoCollection";
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
     new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
 
-let collection: TodoCollection = new TodoCollection("Adam", todos);
+let collection: TodoCollection = new JsonTodoCollection("Adam", todos);
 let showCompleted = true;
 
 function displayTodoList(): void {
